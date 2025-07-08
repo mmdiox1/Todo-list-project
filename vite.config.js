53a52d4226f36b00673df35b3bfaeb2226c10d48
mohammad reza <mmdioxemi@gmail.com>
2025-07-08 11:27:53 +0300

    deploying page on GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  base: "/Todo-list-project/", // مسیر دقیق مخزن گیت‌هاب
 });
